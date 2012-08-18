4dfe735a0edc2bdeff5d31eaf7a76ff15119a4b5
John Richter <johndavidrichter@gmail.com>
2012-08-17 19:05:47 -0500

    msg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,10 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,6 +8,23 @@ describe "UserPages" do
 		it {should have_selector('title', text:full_title('Sign up'))}
 	end
 
+	describe "edit" do
+		let(:user) {FactoryGirl.create(:user)}
+		before {visit edit_user_path(user)}
+
+		describe "page" do
+			it {should have_selector('h1', text:"Update your profile")}
+			it {should have_selector('title', text: "Edit user")}
+			it {should have_link('change', href: 'http://gravatar.com/emails')}
+		end
+
+		describe "with invalid information" do
+			before {click_button "Save changes"}
+			it {should have_content('error')}
+		end
+	end
+
+
 	describe "profile page" do
 		let (:user) {FactoryGirl.create(:user)}
 		before {visit user_path(user)}
